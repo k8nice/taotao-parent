401bf7430510d9ac69d817bfade623d9e7919ec0
ningh <996099092@qq.com>
2019-06-02 17:28:12 +0800

    修改redis依赖版本，引入redis依赖

--- taotao-portal/src/main/java/com/nice/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/nice/portal/controller/IndexController.java
@@ -29,9 +29,9 @@ public class IndexController {
         return "index";
     }
 
-    @RequestMapping(value = "/httpclient/post",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE+";charset=utf-8")
+    @RequestMapping(value = "/httpclient/post",method = RequestMethod.POST,produces = MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
     @ResponseBody
     public String testPost(String username,String password){
-        return "{username:" + username + ",password:" + password + "}";
+        return "username:" + username + ",password:" + password ;
     }
 }
--- taotao-rest/src/main/java/com/nice/rest/service/ContentService.java
+++ taotao-rest/src/main/java/com/nice/rest/service/ContentService.java
@@ -4,6 +4,9 @@ import com.nice.pojo.TbContent;
 
 import java.util.List;
 
+/**
+ * @author ningh
+ */
 public interface ContentService {
 
     List<TbContent> getContentList(long contentCid);
